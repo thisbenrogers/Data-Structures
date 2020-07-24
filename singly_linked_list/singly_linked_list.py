fc1c1e680a29dca5b17276f4d7de6acee091dcbc
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-07-23 20:01:00 -0500

    Adds LinkedList Class/Methods

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -11,3 +11,64 @@ class Node:
     def set_next(self, new_next):
         # set this node's next_node reference to the passed in node
         self.next_node = new_next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self. tail = None
+        self.length = 0
+
+    def add_to_head(self, value):
+        new_node = Node(value, self.head)
+        self.head = new_node
+        if self.length == 0:
+            self.tail = new_node
+        self.length += 1
+
+    def add_to_tail(self, value):
+        new_node = Node(value)
+        if self.head is None and self.tail is None:
+            self.head = new_node
+        else:
+            self.tail.set_next(new_node)
+        self.tail = new_node
+        self.length += 1
+
+    def contains(self, value):
+        pass
+
+    def remove_head(self):
+        if self.head is None:
+            return None
+        elif self.head.get_next() is None:
+            value = self.head.get_value()
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return value
+        else:
+            value = self.head.get_value()
+            self.head = self.head.get_next()
+            self.length -= 1
+            return value
+            
+
+    def get_max(self):
+        if self.length == 0:
+            return None
+        else:
+            global current_node
+            global next_node
+            global biggest
+            current_node = self.head
+            next_node = self.head.get_next()
+            biggest = current_node.value
+            while current_node.get_next() != None:
+                if next_node.get_value() > current_node.get_value():
+                    biggest = next_node.get_value()
+                    current_node = next_node
+                    next_node = current_node.get_next()
+                else:
+                    current_node = next_node
+                    next_node = current_node.get_next()
+            return biggest
