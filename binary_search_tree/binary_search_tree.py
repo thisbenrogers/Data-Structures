41aceddf8f3af352a3b5b187b42691cd155b8b4e
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-07-27 23:15:32 -0500

    Adds insert() and contains() functions, comments out premature references to code that will be needed later

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,12 +17,29 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if self.value < value:
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+        else:
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        while self is not None:
+            if self.value == target:
+                return True
+            elif self.value < target:
+                self = self.right
+            elif self.value > target:
+                self = self.left
+        return False
+            
 
     # Return the maximum value found in the tree
     def get_max(self):
@@ -30,6 +47,10 @@ class BSTNode:
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
+        pass
+
+    # Part 1 Stretch -----------
+    def delete(self, value):
         pass
 
     # Part 2 -----------------------
@@ -63,7 +84,7 @@ class BSTNode:
 """
 This code is necessary for testing the `print` methods
 """
-bst = BinarySearchTree(1)
+bst = BSTNode(1)
 
 bst.insert(8)
 bst.insert(5)
@@ -73,13 +94,13 @@ bst.insert(3)
 bst.insert(4)
 bst.insert(2)
 
-bst.bft_print()
-bst.dft_print()
+# bst.bft_print()
+# bst.dft_print()
 
-print("elegant methods")
-print("pre order")
-bst.pre_order_dft()
-print("in order")
-bst.in_order_dft()
-print("post order")
-bst.post_order_dft()  
+# print("elegant methods")
+# print("pre order")
+# bst.pre_order_dft()
+# print("in order")
+# bst.in_order_dft()
+# print("post order")
+# bst.post_order_dft()  
